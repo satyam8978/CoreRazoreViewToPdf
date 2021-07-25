428c9405c337ad0376393526560dff4b638d3227
satyam jatla <satyam0297@gmail.com>
2021-07-25 13:22:55 +0530

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorPdfTask.Models;
+using Rotativa.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPdfTask.Controllers
+{
+    public class WeatherForecastController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Weather> objweathers = new List<Weather>();
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Weather weathers = new Weather();
+                weathers.Date = DateTime.Now.AddDays(i);
+                weathers.TemperatureC = 21 + i;
+                weathers.TemperatureF = 32 + (int)(i / 0.5556);
+                weathers.Summary = "Test Satyam";
+
+                objweathers.Add(weathers);
+            }
+            return new ViewAsPdf(objweathers);
+        }
+   
+    }
+}
